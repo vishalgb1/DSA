9936effb1f8e9b2bc984e373b26870f1965345aa
Vishal <vishal.byahatti@gmail.com>
2022-07-07 23:07:36 +0530

    Add files via upload

--- QuickSort.java
+++ QuickSort.java
@@ -1,12 +1,12 @@
 package com.sorting;
 
 public class QuickSort {
-
+	
 	public static int partition(int arr[], int low, int high) {
 		int pivot = arr[high];
-		int i = low - 1;
-		for (int j = low; j < high; j++) {
-			if (arr[j] < pivot) {
+		int i=low-1;
+		for(int j=low;j<high;j++) {
+			if(arr[j] < pivot) {
 				i++;
 				int temp = arr[i];
 				arr[i] = arr[j];
@@ -19,25 +19,27 @@ public class QuickSort {
 		arr[high] = temp;
 		return i;
 	}
-
+	
 	public static void quickSort(int arr[], int low, int high) {
-		if (low < high) {
+		if(low < high) {
 			int pidx = partition(arr, low, high);
-			quickSort(arr, 0, pidx - 1);
+			quickSort(arr, 0, pidx-1);
 			quickSort(arr, pidx + 1, high);
 		}
 	}
-
-	public static void print(int arr[]) {
-		for (int i = 0; i < arr.length; i++) {
+	
+	public static void print(int arr[]){
+		for(int i=0;i<arr.length;i++) {
 			System.out.print(arr[i] + " ");
 		}
 	}
 
-	public static void main(String args[]) {
+
+	public static void main (String args[]) {
 		int arr[] = { 2, 5, 6, 4, 9, 25, -1 };
 		int n = arr.length;
-		quickSort(arr, 0, n - 1);
+		quickSort(arr,0,n-1);
 		print(arr);
+
 	}
 }
