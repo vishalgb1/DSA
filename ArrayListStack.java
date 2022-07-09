9764c8047f0e8bb819ccf6133e172ad59b4e2acb
Vishal <vishal.byahatti@gmail.com>
2022-07-09 16:52:47 +0530

    Add files via upload

--- ArrayListStack.java
+++ ArrayListStack.java
@@ -3,10 +3,9 @@ package com.stack;
 import java.util.ArrayList;
 
 public class ArrayListStack {
-
+	
 	static class Stack{
 		static ArrayList<Integer> list = new ArrayList<>();
-		
 		public static boolean isEmpty() {
 			return list.size() == 0;
 		}
@@ -19,7 +18,7 @@ public class ArrayListStack {
 			if(isEmpty()) {
 				return -1;
 			}
-			int top = list.get(list.size() - 1);
+			int top = list.get(list.size() -1);
 			list.remove(list.size() - 1);
 			return top;
 		}
@@ -29,21 +28,19 @@ public class ArrayListStack {
 				return -1;
 			}
 			return list.get(list.size() - 1);
-		}
-	
-	
+		}		
+	}
 	public static void main (String args[]) {
 		Stack stack = new Stack();
 		stack.push(1);
 		stack.push(2);
 		stack.push(3);
 		stack.push(4);
-		
-		while( !stack.isEmpty()) {
-			System.out.println(peek());
-			pop();
-		}	
+	
+	
+		while( ! stack.isEmpty()) {
+			System.out.println(stack.peek());
+			stack.pop();
+		}
 	}
-	}
-
 }
